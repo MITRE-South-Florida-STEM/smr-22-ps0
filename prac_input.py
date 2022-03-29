01f74dac25023440c223ae4d0679a437d5d8b20f
dlee-mitre <85962425+dlee-mitre@users.noreply.github.com>
2022-03-28 20:15:48 -0400

    Create prac_input.py

---
+++ prac_input.py
@@ -0,0 +1,13 @@
+# please use the code below to run your code
+
+# replace the following line to prompt user for first number
+print('first question')
+
+# replace the following line to prompt user for second number
+print('second question')
+
+# replace the following line to compute exponent
+print('compute something')
+
+# replace the following line to print solution
+print('print solution')
